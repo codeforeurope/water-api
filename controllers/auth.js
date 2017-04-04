4cbb2cda6625908ccbd4933db3a134226a7816b2
Duncan Healy <duncan.healy@gmail.com>
2017-04-04 10:20:51 +0100

    auth setup

---
+++ controllers/auth.js
@@ -0,0 +1,10 @@
+/*
+//http://apprize.info/javascript/cors/8.html
+// TODO CORS
+// res.header("Access-Control-Allow-Origin", '*')
+// res.header('Access-Control-Allow-Headers', 'refreshToken,accessToken')
+
+
+
+*/
+// TODO 
