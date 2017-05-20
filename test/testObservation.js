6ffc7fd0e55d3b103cc71fca93e90c521653caae
milovanderlinden <milo@dogodigi.net>
2017-05-20 20:04:13 +0200

    Trying to save objects

--- test/testObservation.js
+++ test/testObservation.js
@@ -29,16 +29,24 @@ describe('testing inserting i18n codes into the database', function() {
   var mgl = models.Uom.model({
     value: {en: 'mg/L', nl: 'mg/L'},
     label: {en: 'mg/L', nl: 'mg/L'},
-    definition: {en: 'Milligram per liter, Mass concentration unit. Conversion to SI unit: 1 kg/m3 = 10^3 mg/L'}
+    definition: {en: 'Milligram per liter, Mass concentration unit. Conversion to SI unit: 1 kg/m3 = 10^3 mg/L'},
+    entered_by: user
   });
+
+  mgl.save(function (err, mgl, count) {
+    mgl = mgl;
+  });
+
   var potassium = models.Code.model({ standard: 'CAS', value: '7440-09-7', label: {en: 'Potassium', de: 'Kalium'} });
   var testObservation = models.Observation.model({ value: 200, uom: mgl, code: potassium, entered_by: user });
-  testObservation.save();
+
   describe('Observation should pass', function() {
     it('it should be valid', function(done) {
-      var out = testObservation.toJSONLocalizedOnly('nl','en');
-      should.equal(out.code.label, 'Potassium');
-      done();
+      testObservation.save(function(err, result, count){
+        var out = result.toJSONLocalizedOnly('nl','en');
+        should.equal(out.code.label, 'Potassium');
+        done();
+      });
     });
   });
 });
