d9790a2466de198d17caac235ff4b50b730a7a70
milovanderlinden <milo@dogodigi.net>
2017-06-08 13:59:22 +0200

    Added /bin/bash

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sed -i "s^__MONGO_SERVER__^$MONGO_SERVER^g" /water-api/config.json
 sed -i "s^__SECRET__^$SECRET^g" /water-api/config.json
 sed -i "s^__DB_PREFIX__^$DB_PREFIX^g" /water-api/config.json
