0861a382244fcfaf6e33015b1b4947b27b81b7bd
milovanderlinden <milo@dogodigi.net>
2017-06-23 15:03:22 +0200

    Typo in Docker

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ sed -i "s^__SECRET__^$SECRET^g" /water-api/config/config.json
 sed -i "s^__DB_PREFIX__^$DB_PREFIX^g" /water-api/config/config.json
 
 cd /water-api
-cd /assets
+cd assets
 git pull
 cd ..
 npm start
